ff8ca103f0cbe2861edf417a0d6daf2724bcf00b
chetan somana <chetan.somana@gmail.com>
2024-07-23 19:25:55 +0200

    gpio_second_commit

--- tests/test_gpio_input.sv
+++ tests/test_gpio_input.sv
@@ -7,7 +7,6 @@ class gpio extends gpio_base;
 
   if(pwm_gpio_seq.val == 0 ) begin
     read(ldo1_reg_val) = 4'b1011; //2.5v
-    read(ldo2_reg_val) = 4'b1110; //3v
     read(buck_reg_val) = 4'b1111; //3.1v
   end
   else begin
