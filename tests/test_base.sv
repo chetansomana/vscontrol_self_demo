2b23748177d6bcac5202198055ffff7b39d4fc4d
chetan somana <chetan.somana@gmail.com>
2024-07-23 20:59:48 +0200

    added display in test base

--- tests/test_base.sv
+++ tests/test_base.sv
@@ -4,8 +4,10 @@ class base_test;
   ldo1_volt = 5'b1_0010;
   sw1_voly = 5'b1_0101;
 
+  $display("creating merge fault");
   pwm_seq pwm_period_seq;
   wdog_seq wdog_error_seq;
+  $display("merge fault");
 
   pwm_period_seq.start(m_seqr);
   wdog_error_seq.start(m_seqr);
